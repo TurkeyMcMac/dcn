86eae512e266ba3b1925c19f1339c0047698067c
TurkeyMcMac <jwmhjwmh@gmail.com>
2017-05-19 20:20:02 -0400

    added lexical scoping

--- src/jwmh/dcn/CollectiveCapsule.java
+++ src/jwmh/dcn/CollectiveCapsule.java
@@ -2,12 +2,18 @@ package jwmh.dcn;
 
 import java.util.AbstractMap;
 import java.util.AbstractMap.SimpleEntry;
+import java.util.AbstractSet;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 abstract class CollectiveCapsule<T> extends Capsule<T> {
+	
+	private static final Map<Object, Object> VARS = new HashMap<>();
 	
 	protected CollectiveCapsule(Character start, Character finish, String selector) {
 		super(start, finish, selector);
@@ -15,8 +21,8 @@ abstract class CollectiveCapsule<T> extends Capsule<T> {
 	
 	@SuppressWarnings("unchecked")
 	protected final ValueEnd evaluate(String capsule) {
-		//storage for variables within this scope
-		Map<Object, Object> vars = new HashMap<>();
+		//list of variables marked as local
+		Set<Object> localVars = new HashSet<>();
 		//list of processed values, to be filled
 	    List<Object> valueList = new ArrayList<>();
 	    //the position of the end of the capsule, to be determined
@@ -27,23 +33,32 @@ abstract class CollectiveCapsule<T> extends Capsule<T> {
 	        //check if currentChar is the beginning of a sub-capsule
 	        Capsule<?> subCapsule = capsuleStarts.get(currentChar);
 	        if (subCapsule != null) {
-	            //pass sub-capsule string to sub-capsule
 	            ValueEnd subValue = (Capsule<T>.ValueEnd) subCapsule.evaluate(capsule.substring(i + 1));
 	            if (!(subCapsule instanceof Ignored)) {
 	            	if (subCapsule instanceof GetterCapsule) {
-	            		valueList.add(vars.get(subValue.value));
+	            		valueList.add(VARS.get(subValue.value));
 	            	} else {
 	            		valueList.add(subValue.value);
 	            	}
 	            } else if (subCapsule instanceof SetterCapsule) {
 	            	AbstractMap.SimpleEntry<Object, Object> var = (SimpleEntry<Object, Object>)subValue.value;
-	            	vars.put(var.getKey(), var.getValue());
+	            	Object name = var.getKey();
+	            	Object value = var.getValue();
+	            	if (VARS.containsKey(name)) {
+	            		VARS.put(name, value);
+	            	} else {
+	            		VARS.put(name, value);
+	            		localVars.add(name);
+	            	}
 	            }
 	            i += subValue.terminator;
 	        } else if (currentChar == capsuleBorders.get(START)) {
 	        	terminator = i;
 	        	break;
 	        }
+	    }
+	    for (Object v : localVars) {
+	    	VARS.remove(v);
 	    }
 	    return new ValueEnd(processList((List<Object>)valueList), terminator);
 	}
